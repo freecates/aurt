e3ce9de72a64abb7ae7f15cc247f916cd0556005
Ramon Gil <ramon.gil@deporvillage.com>
2020-10-03 19:36:13 +0200

    master explicit width

--- components/Awards.js
+++ components/Awards.js
@@ -5,16 +5,16 @@ const Awards = (props) => {
   return (
     <AwardsStyles>
       {ruta == '/' || ruta == '/ca' || ruta == '/en' ? (
-        <img className={'home'} loading='lazy' src='/static/awards-home.svg' alt='aürt awards' height='130' width='auto' />
+        <img className={'home'} loading='lazy' src='/static/awards-home.svg' alt='aürt awards' height='130' width='362' />
       ) : ruta == '/artur' || ruta == '/ca/artur' || ruta == '/en/artur' ? (
         <img className={'artur'} loading='lazy'
           src='/static/awards-artur-aurt.svg'
           alt='artur aürt awards'
           height='100'
-          width='auto'
+          width='279'
         />
       ) : (
-        <img className={'others'} loading='lazy' src='/static/awards-aurt.svg' alt='aürt awards' height='100' width='auto' />
+        <img className={'others'} loading='lazy' src='/static/awards-aurt.svg' alt='aürt awards' height='100' width='172' />
       )}
     </AwardsStyles>
   );
