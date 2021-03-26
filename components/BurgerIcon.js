76021b5108d9aa31a36ac301a64e9797fb18b6ac
Ramon Gil <ramon.gil@deporvillage.com>
2021-03-26 12:21:18 +0100

    with-next-10-0-9 , upgraded nextjs version

--- components/BurgerIcon.js
+++ components/BurgerIcon.js
@@ -1,7 +1,9 @@
-export default ({ open, ...props }) => (
+const BurgerIcons = ({ open, ...props }) => (
   <div className={open ? 'burger-menu open' : 'burger-menu'} {...props}>
-    <div className="bar1" key="b1" />
-    <div className="bar2" key="b2" />
-    <div className="bar3" key="b3" />
+    <div className='bar1' key='b1' />
+    <div className='bar2' key='b2' />
+    <div className='bar3' key='b3' />
   </div>
-)
+);
+
+export default BurgerIcons;
