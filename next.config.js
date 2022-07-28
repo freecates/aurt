000da9c07ecc42f69d051139e672a3f3bb8a6262
Ramon Gil <ramon.gil@deporvillage.com>
2022-07-28 08:11:16 +0200

    master [aurt] Added redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 // next.config.js
 module.exports = {
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/pack',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/en/pack',
+        destination: '/en',
+        permanent: true,
+      },
+      {
+        source: '/ca/pack',
+        destination: '/ca',
+        permanent: true,
+      },
+    ]
+  },
 };
