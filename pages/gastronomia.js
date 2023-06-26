4558699e9bf9b8037ad027161dc61c27178b141a
Ramon <ramongil@ramongil.cat>
2023-06-26 18:58:58 +0200

    feat(aurt): [gastronomia-from-cms] Added gastronomy content from cms (#52)

--- pages/gastronomia.js
+++ pages/gastronomia.js
@@ -1,79 +1,32 @@
-import InnerLayout from '../components/InnerLayout';
-import Title from '../components/styles/Title';
-import SubTitle from '../components/styles/SubTitle';
-import TextBlock from '../components/styles/TextBlock';
-import TextSeparator from '../components/styles/TextSeparator';
-import ItemStyles from '../components/styles/ItemStyles';
-import Head from 'next/head';
+import Gastronomies from '@components/Gastronomies';
+import api from '@libs/api.js';
 
-const Page = (props) => (
-  <InnerLayout mainlayout>
-    <Head>
-      <title>AÜRT Restaurant | {props.post.title}</title>
-      <meta name='description' content={props.post.secondTextBlock} />
-      <link
-        rel='canonical'
-        href={`https://www.aurtrestaurant.com${props.pathname}`}
-      />
-      <link
-        rel='alternate'
-        hrefLang={'ca'}
-        href={`https://www.aurtrestaurant.com${props.pathname.replace(
-          /\//g,
-          '/ca/'
-        )}`}
-      />
-      <meta
-        property='og:url'
-        content={`https://www.aurtrestaurant.com${props.pathname}`}
-      />
-      <meta property='og:type' content='article' />
-      <meta
-        property='og:title'
-        content={`AÜRT Restaurant | ${props.post.title}`}
-      />
-      <meta property='og:description' content={props.post.secondTextBlock} />
-      <meta
-        property='og:image'
-        content={
-          'https://www.aurtrestaurant.com/static/icons/og-image-aurt-web.png'
-        }
-      />
-      <meta property='og:image:width' content='1024' />
-      <meta property='og:image:height' content='1024' />
-    </Head>
-    <ItemStyles>
-      <Title>{props.post.title}</Title>
-      <SubTitle
-        dangerouslySetInnerHTML={{
-          __html: props.post.subTitle,
-        }}
-      />
-      <SubTitle>{props.post.secondSubTitle}</SubTitle>
-      <TextSeparator>
-        <div className='here' />
-      </TextSeparator>
-      <TextBlock
-        dangerouslySetInnerHTML={{
-          __html: props.post.firstTextBlock,
-        }}
-      />
-      <TextBlock
-        dangerouslySetInnerHTML={{
-          __html: props.post.secondTextBlock,
-        }}
-      />
-    </ItemStyles>
-  </InnerLayout>
-);
+const Gastronomia = (props) => {
+  const {
+    gastronomia: data,
+    titol 
+  } = props.singleFromCMS[0].acf;
+  return (
+    <Gastronomies
+      data={data}
+      titol={titol}
+      ruta={props.pathname}
+    />
+  );
+};
 
-export async function getStaticProps() {
-  const res = await fetch(`https://aurt-data.vercel.app/data/gastronomia.json`);
-  const post = await res.json();
+export const getStaticProps = async () => {
+  const id = 775;
+  const [singleFromCMS] = await Promise.all([
+    api.singleFromCMS.getData(id, 'gastronomy'),
+  ]);
 
   return {
-    props: { post }, // will be passed to the page component as props
+    props: {
+      singleFromCMS: [singleFromCMS],
+    },
+    revalidate: 1,
   };
-}
+};
 
-export default Page;
+export default Gastronomia;
