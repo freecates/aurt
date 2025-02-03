b04c56d6edf314b68cf5a73728c2f53513019fa4
Ramon <ramongil@ramongil.cat>
2025-02-03 20:10:58 +0100

    chore(components): [aurt-content-changes-01022025] Added visible state for banner home

--- components/BannerHome.js
+++ components/BannerHome.js
@@ -1,10 +1,30 @@
+import React from 'react';
 import styled from 'styled-components';
 
 const StyleBanner = styled.div`
+  display: ${(props) => (props.visible ? 'block' : 'none')};
+  visibility: ${(props) => (props.visible ? 'visible' : 'hidden')};
+  opacity: ${(props) => (props.visible ? 1 : 0)};
   position: absolute;
   top: 19rem;
   left: 0;
   width: 100%;
+  span {
+    display: block;
+    cursor: pointer;
+    position: relative;
+    z-index: 2001;
+    width: 25px;
+    height: 25px;
+    left: calc(50% + 150px);
+    top: 40px;
+    @media (min-width: 1024px) {
+      width: 25px;
+      height: 25px;
+      left: calc(50% + 275px);
+      top: 50px;
+    }
+  }
   a {
     display: inline;
     position: relative;
@@ -26,18 +46,22 @@ const StyleBanner = styled.div`
   }
 `;
 
-const BannerHome = () => (
-  <StyleBanner>
-    <a href='https://www.covermanager.com/reserve/module_restaurant/restaurante-aurtx/spanish?start=2025-02-24&end=2025-02-25'>
-      <img
-        loading='lazy'
-        src='/static/banner-huniik-es.gif'
-        alt='Og Image AURT Web'
-        width='550'
-        height='537'
-      />
-    </a>
-  </StyleBanner>
-);
+const BannerHome = () => {
+  const [isVisble, setIsVisible] = React.useState(true);
+  return (
+    <StyleBanner visible={isVisble}>
+      <span onClick={() => setIsVisible(false)} />
+      <a href='https://www.covermanager.com/reserve/module_restaurant/restaurante-aurtx/spanish?start=2025-02-24&end=2025-02-25'>
+        <img
+          loading='lazy'
+          src='/static/banner-huniik-es.gif'
+          alt='Og Image AURT Web'
+          width='550'
+          height='537'
+        />
+      </a>
+    </StyleBanner>
+  );
+};
 
 export default BannerHome;
